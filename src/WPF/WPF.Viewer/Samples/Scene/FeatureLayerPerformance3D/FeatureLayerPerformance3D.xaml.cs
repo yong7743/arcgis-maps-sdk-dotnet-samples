8a34a8c8d37ff42eb3cb02391955c781a7899a7a
Yongwei Kang <ykang@esri.com>
2024-08-29 15:32:30 +0800

    add single view

--- src/WPF/WPF.Viewer/Samples/Scene/FeatureLayerPerformance3D/FeatureLayerPerformance3D.xaml.cs
+++ src/WPF/WPF.Viewer/Samples/Scene/FeatureLayerPerformance3D/FeatureLayerPerformance3D.xaml.cs
@@ -7,12 +7,15 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
+using ArcGIS.Helpers;
 using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.UI.Controls;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
@@ -32,11 +35,31 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
         {
             InitializeComponent();
 
-            // Initialize the sample.
             Initialize();
+
+            UrlTextBox.Text = _featureService;
         }
 
         private void Initialize()
+        {
+            // Call a function to set up the AuthenticationManager for OAuth.
+            ArcGISLoginPrompt.SetChallengeHandler();
+
+            // Initialize the sample.
+            InitializeSingleView();
+            InitializeComparedView();
+
+            // Start monitoring the performance
+            StartMonitoring();
+        }
+
+        private void InitializeSingleView()
+        {
+            MySceneView.Scene = new Scene();
+        }
+
+        #region Comapred view
+        private void InitializeComparedView()
         {
             // Disable 'flick' gesture
             // This is the most straightforward way to prevent the 'flick' animation on one view from competing with user interaction on the other
@@ -56,20 +79,6 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
 
             // Create the scene for displaying the feature layer in dynamic mode.
             Scene dynamicScene = new Scene();
-
-            // Create and add the static layer.
-            FeatureLayer staticLayer = new FeatureLayer(new ServiceFeatureTable(new Uri(_featureService)))
-            {
-                RenderingMode = FeatureRenderingMode.Static
-            };
-            staticScene.OperationalLayers.Add(staticLayer);
-
-            // Create and add the dynamic layer.
-            FeatureLayer dynamicLayer = new FeatureLayer(new ServiceFeatureTable(new Uri(_featureService)))
-            {
-                RenderingMode = FeatureRenderingMode.Dynamic
-            };
-            dynamicScene.OperationalLayers.Add(dynamicLayer);
 
             // Add the scenes to the scene views.
             MyStaticSceneView.Scene = staticScene;
@@ -93,10 +102,7 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
             }
 
             // Update the viewpoint on the other view
-            if (otherView.IsVisible)
-            {
-                otherView.SetViewpoint(sendingView.GetCurrentViewpoint(ViewpointType.CenterAndScale));
-            }
+            otherView.SetViewpoint(sendingView.GetCurrentViewpoint(ViewpointType.CenterAndScale));
         }
 
         private void OnViewpointChanged(object sender, EventArgs e)
@@ -111,11 +117,6 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
                 // If the MyStaticSceneView sent the event, update the MyDynamicSceneView's viewpoint
                 if (sendingView == MyStaticSceneView)
                 {
-                    if (!MyDynamicSceneView.IsVisible)
-                    {
-                        return;
-                    }
-
                     // Get the viewpoint
                     Viewpoint updateViewpoint = MyStaticSceneView.GetCurrentViewpoint(ViewpointType.CenterAndScale);
 
@@ -124,11 +125,6 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
                 }
                 else // Else, update the MyStaticSceneView's viewpoint
                 {
-                    if (!MyStaticSceneView.IsVisible)
-                    {
-                        return;
-                    }
-
                     // Get the viewpoint
                     Viewpoint updateViewpoint = MyDynamicSceneView.GetCurrentViewpoint(ViewpointType.CenterAndScale);
 
@@ -137,21 +133,170 @@ namespace ArcGIS.WPF.Samples.FeatureLayerPerformance3D
                 }
             }
         }
+        #endregion Comapred view
 
-        bool _enableStaticSceneView = true;
-        bool _enableDynamicSceneView = true;
-        private void MyStaticSceneViewButton_Click(object sender, RoutedEventArgs e)
+        #region Performance monitoring
+        private Timer _timer;
+        private void StartMonitoring()
         {
-            _enableStaticSceneView = !_enableStaticSceneView;
-            MyStaticSceneView.Visibility = _enableStaticSceneView ? Visibility.Visible : Visibility.Collapsed;
-            MyStaticSceneViewButton.Content = _enableStaticSceneView ? "Hide" : "Show";
+            _timer = new Timer(2000); // Update every 2 seconds
+            _timer.Elapsed += UpdatePerformanceData;
+            _timer.Start();
         }
 
-        private void MyDynamicSceneViewButton_Click(object sender, RoutedEventArgs e)
+        private async void UpdatePerformanceData(object sender, ElapsedEventArgs e)
         {
-            _enableDynamicSceneView = !_enableDynamicSceneView;
-            MyDynamicSceneView.Visibility = _enableDynamicSceneView ? Visibility.Visible : Visibility.Collapsed;
-            MyDynamicSceneViewButton.Content = _enableDynamicSceneView ? "Hide" : "Show";
+            var (cpuUsage, memoryUsage) = await GetCpuAndMemoryUsage();
+
+            Dispatcher.Invoke(() =>
+            {
+                CpuUsageTextBlock.Text = $"{cpuUsage:F2}%";
+                MemoryUsageTextBlock.Text = $"{memoryUsage:F2} MB";
+            });
+        }
+
+        private async Task<(double, double)> GetCpuAndMemoryUsage()
+        {
+            var process = Process.GetCurrentProcess();
+            var cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName, true);
+            var ramCounter = new PerformanceCounter("Process", "Private Bytes", process.ProcessName, true);
+
+            cpuCounter.NextValue();
+            ramCounter.NextValue();
+
+            await Task.Delay(500);
+
+            var cpuUsage = Math.Round(cpuCounter.NextValue() / Environment.ProcessorCount, 2);
+            var memoryUsage = Math.Round(ramCounter.NextValue() / 1024 / 1024, 2); // MB
+
+            return (cpuUsage, memoryUsage);
+        }
+        #endregion Performance monitoring
+
+        long _numberOfFeatures = 0;
+        private async void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            _featureService = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(_featureService))
+            {
+                MessageBox.Show("Please enter a valid feature service URL.");
+                return;
+            }
+
+            try
+            {
+                LoadDataButton.IsEnabled = false;
+                if (_isComparedView)
+                {
+                    // Create and add the static layer.
+                    var staticLayer = await CreateFeatureLayerAsync(_featureService, FeatureRenderingMode.Static);
+                    MyStaticSceneView.Scene.OperationalLayers.Add(staticLayer);
+
+                    // Create and add the dynamic layer.
+                    var dynamicLayer = await CreateFeatureLayerAsync(_featureService, FeatureRenderingMode.Dynamic);
+                    MyDynamicSceneView.Scene.OperationalLayers.Add(dynamicLayer);
+
+                    // Get Feature numbers
+                    var count = await QueryFearureCountAsync(dynamicLayer.FeatureTable);
+                    _numberOfFeatures += count;
+                    LayersInfoTextBlock.Text = $"Number of features: {_numberOfFeatures}";
+
+                    // Fly to the full extent of the layer.
+                    await MyDynamicSceneView.SetViewpointAsync(new Viewpoint(dynamicLayer.FullExtent), TimeSpan.FromSeconds(3));
+                }
+                else
+                {
+                    var renderingMode = (FeatureRenderingMode)RenderingModeComboBox.SelectedIndex;
+                    var layer = await CreateFeatureLayerAsync(_featureService, renderingMode);
+                    MySceneView.Scene.OperationalLayers.Add(layer);
+
+                    // Get Feature numbers
+                    var count = await QueryFearureCountAsync(layer.FeatureTable);
+                    _numberOfFeatures += count;
+                    LayersInfoTextBlock.Text = $"Number of features: {_numberOfFeatures}";
+
+                    // Fly to the full extent of the layer.
+                    await MySceneView.SetViewpointAsync(new Viewpoint(layer.FullExtent), TimeSpan.FromSeconds(3));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error loading data");
+            }
+            finally
+            {
+                LoadDataButton.IsEnabled = true;
+            }
+        }
+
+        private async Task<FeatureLayer> CreateFeatureLayerAsync(string url, FeatureRenderingMode renderingMode)
+        {
+            // Create the service feature table.
+            ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable(new Uri(url));
+
+            // Create the feature layer using the service feature table and set the rendering mode.
+            FeatureLayer featureLayer = new FeatureLayer(serviceFeatureTable) { RenderingMode = renderingMode };
+
+            await featureLayer.LoadAsync();
+
+            return featureLayer;
+        }
+
+        private async Task<long> QueryFearureCountAsync(FeatureTable featureTable)
+        {
+            if (featureTable == null)
+            {
+                return 0;
+            }
+
+            // Create the query parameters.
+            QueryParameters queryParameters = new QueryParameters
+            {
+                WhereClause = "1=1",
+                ReturnGeometry = false
+            };
+
+            // Query the feature table.
+            long count = await featureTable.QueryFeatureCountAsync(queryParameters);
+
+            return count;
+        }
+
+        bool _isComparedView = false;
+        private void ComparedViewCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            _isComparedView = ComparedViewCheckBox.IsChecked == true;
+
+            if (_isComparedView)
+            {
+                ComparedView.Visibility = Visibility.Visible;
+                SingleView.Visibility = Visibility.Collapsed;
+                RenderingModeComboBox.SelectedIndex = -1;
+                RenderingModeComboBox.IsEnabled = false;
+            }
+            else
+            {
+                SingleView.Visibility = Visibility.Visible;
+                ComparedView.Visibility = Visibility.Collapsed;
+                RenderingModeComboBox.SelectedIndex = 0;
+                RenderingModeComboBox.IsEnabled = true;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear all data in the views
+            if (_isComparedView)
+            {
+                MyStaticSceneView.Scene.OperationalLayers.Clear();
+                MyDynamicSceneView.Scene.OperationalLayers.Clear();
+            }
+            else
+            {
+                MySceneView.Scene.OperationalLayers.Clear();
+            }
+            LayersInfoTextBlock.Text = "";
+            _numberOfFeatures = 0;
         }
     }
 }
